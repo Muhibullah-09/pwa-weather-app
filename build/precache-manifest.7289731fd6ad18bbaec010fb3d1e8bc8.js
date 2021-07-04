27a58b234052d7b7c184f0f8e2469123da770478
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-07-05 01:44:14 +0500

    bug fixed

---
+++ build/precache-manifest.7289731fd6ad18bbaec010fb3d1e8bc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0857a54ca88c1b68d27ca6ae23aa00ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf386785995824f806f3",
+    "url": "/static/css/main.a56a16e9.chunk.css"
+  },
+  {
+    "revision": "811fd32ed60c5f901aa3",
+    "url": "/static/js/2.ec6ee7fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec6ee7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf386785995824f806f3",
+    "url": "/static/js/main.be828e27.chunk.js"
+  },
+  {
+    "revision": "9477723e8b43b199ec93",
+    "url": "/static/js/runtime-main.ba3ed84e.js"
+  }
+]);
